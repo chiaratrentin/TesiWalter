761139f56a2da1c2b9c901276f9394ed889507df
chiaratrentin <chiaratrentin1984@gmail.com>
2014-04-11 12:00:15 +0200

    added a database

---
+++ tesiwalter/tesiwalter/TRNAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  TRNAppDelegate.m
+//  tesiwalter
+//
+//  Created by Chiara Trentin on 24/03/14.
+//  Copyright (c) 2014 Chiara Trentin. All rights reserved.
+//
+
+#import "TRNAppDelegate.h"
+
+@implementation TRNAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
